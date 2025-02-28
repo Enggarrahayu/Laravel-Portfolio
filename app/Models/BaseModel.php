f40eceb7c94c9f832b3f57d2a0e41c8c360a55e4
Your Name <enggarrahayu64@gmail.com>
2025-02-28 14:24:28 +0700

    feat: initial commit

---
+++ app/Models/BaseModel.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+abstract class BaseModel extends Model
+{
+    protected $keyType = 'string';
+    public $incrementing = false;
+
+    protected static function boot()
+    {
+        parent::boot();
+        static::creating(function ($model) {
+            if (!$model->getKey()) {
+                $model->{$model->getKeyName()} = (string) Str::uuid();
+            }
+        });
+    }
+}
