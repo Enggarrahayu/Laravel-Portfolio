a9b909048ca22c08d90c3e89083b29072fd1c076
Your Name <enggarrahayu64@gmail.com>
2025-02-28 14:33:04 +0700

    feat(core): implement BaseModel with UUID and logging

--- app/Models/BaseModel.php
+++ app/Models/BaseModel.php
@@ -8,15 +8,16 @@ use Illuminate\Support\Str;
 
 abstract class BaseModel extends Model
 {
-    protected $keyType = 'string';
     public $incrementing = false;
+    protected $keyType = 'string';
 
     protected static function boot()
     {
         parent::boot();
+
         static::creating(function ($model) {
-            if (!$model->getKey()) {
-                $model->{$model->getKeyName()} = (string) Str::uuid();
+            if (!$model->id) {
+                $model->id = Str::uuid();
             }
         });
     }
